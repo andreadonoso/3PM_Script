e452ebfcf4a84f198695fe6f85fd70828b89edf0
Andrea Donoso <donosoandrea@icloud.com>
2024-11-08 15:42:26 -0500

    Gmail search query functionality

--- quickstart.py
+++ quickstart.py
@@ -12,12 +12,10 @@ SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googlea
 
 
 def main():
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
+  """Uses the Gmail API to read and search for third-party automated maintenance emails and then creates Google Calendar events using the Google Calendar API.
   """
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
-  """
+  
+  # AUTHORIZATION
   creds = None
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
@@ -39,24 +37,51 @@ def main():
 
   try:
     # Call the Gmail API
-    service = build("gmail", "v1", credentials=creds)
-    results = service.users().labels().list(userId="me").execute()
+    service_gmail = build("gmail", "v1", credentials=creds)
+    results = service_gmail.users().labels().list(userId="me").execute()
     labels = results.get("labels", [])
+    
+     # Perform search query
+    sentFrom = ""
+    label = "sent"
+    subject = ""
+    body = "Dear"
+  
+    f = f'from:"{sentFrom}"' if sentFrom else ''
+    l = f'label:"{label}"' if label else ''
+    s = f'subject:"{subject}"' if subject else ''
+    searchQuery = f'{f} {l} {s} {body}'
+    print("\n\nYour search query: " + searchQuery)
+    messages = service_gmail.users().messages().list(userId="me", q=searchQuery, maxResults=500).execute()
+    
+    if 'messages' not in messages or (messages['resultSizeEstimate'] <= 0):
+      print("No messages found")
+      return 
+    print("-----------------------------------------------------------------------------------------------------------------------------\n")
+    print(f"Quantity: {messages['resultSizeEstimate']} \t\t\t\t\t Max: 500")
+    for message in messages['messages']:
+      print(message)
+    print("\n-----------------------------------------------------------------------------------------------------------------------------\n\n")
+    
+    
+    # For each correct email create an event
+    # Go through events and merge if needed (events have unique id's)
+    
 
-    if not labels:
-      print("No labels found.")
-      return
-    print("Labels:")
-    for label in labels:
-      print(label["name"])
+    # if not labels:
+    #   print("No labels found.")
+    #   return
+    # print("Labels:")
+    # for label in labels:
+    #   print(label["name"])
 
     
     # Call the Calendar API
-    service = build("calendar", "v3", credentials=creds)
+    service_gcal = build("calendar", "v3", credentials=creds)
     now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
     print("Getting the upcoming 10 events")
     events_result = (
-        service.events()
+        service_gcal.events()
         .list(
             calendarId="primary",
             timeMin=now,
@@ -68,16 +93,14 @@ def main():
     )
     events = events_result.get("items", [])
 
-    if not events:
-      print("No upcoming events found.")
-      return
+    # if not events:
+    #   print("No upcoming events found.")
+    #   return
 
     # Prints the start and name of the next 10 events
-    for event in events:
-      start = event["start"].get("dateTime", event["start"].get("date"))
-      print(start, event["summary"])
-
-
+    # for event in events:
+    #   start = event["start"].get("dateTime", event["start"].get("date"))
+    #   print(start, event["summary"])
 
   except HttpError as error:
     # TODO(developer) - Handle errors from gmail API.
